29a19bcbc3d876f48b06a8a424494afee5d68407
Daniel Bertollotto <daniel.bertollotto@nscinfo.com.br>
2022-01-20 14:11:10 -0300

    Commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Clock/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Clock from './components/Clock.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Clock
   }
 }
 </script>
@@ -21,6 +20,10 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+}
+body{
+  margin: 0;
+  -webkit-app-region: drag;
 }
 </style>
---
+++ src/components/Clock.vue
@@ -0,0 +1,35 @@
+<template>
+    <h1>{{ this.time }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      time: null
+    }
+  },
+  methods: {
+    setTime(){
+      setInterval(() => {
+      this.time = new Date().toLocaleTimeString()
+      }, 1000)
+    }
+  },
+  mounted() {
+    this.setTime()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  maring: 0;
+  padding: 0;
+}
+</style>
